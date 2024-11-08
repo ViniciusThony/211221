4a70cc7711c7e3c2095bcbc795ced123cbc6f5dd
ViniciusThony <141239437+ViniciusThony@users.noreply.github.com>
2024-11-08 08:42:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é um "BUG?',
+    'erro no código de um programa que provoca seu mau funcionamento.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa a sigla html?',
+    'abreviação para a expressão inglesa HyperText Markup Language'
+)
+
+    criaCartao(
+    'Lingua inglesa',
+    'Como se diz Vai em Inglês?',
+    'Vai em ingles é Go (GOU)'
+)
